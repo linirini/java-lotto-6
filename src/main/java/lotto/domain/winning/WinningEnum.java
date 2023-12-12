8f650f9bb35dea441e21f4a34806b3d96076357b
linirini <2001yerin@naver.com>
2023-12-12 13:37:52 +0900

    feat : implement winning enums

--- src/main/java/lotto/domain/winning/WinningEnum.java
+++ src/main/java/lotto/domain/winning/WinningEnum.java
@@ -1,21 +1,28 @@
 package lotto.domain.winning;
 
 public enum WinningEnum {
-    ;
 
+    FIRST_PRIZE(6, 2000000000),
+    SECOND_PRIZE(5, 30000000),
+    THIRD_PRIZE(5, 1500000),
+    FOURTH_PRIZE(4, 50000),
+    FIFTH_PRIZE(3, 5000);
+
+    private static final String WINNING_WITH_BONUS_RESULT_PREFIX = "개 일치, 보너스 볼 일치 (";
     private static final String WINNING_RESULT_PREFIX = "개 일치 (";
     private static final String WINNING_RESULT_SUFFIX = "원) - ";
     private int sameNumberCount;
     private int prize;
-    private int winningCount;
 
-    WinningEnum(int sameNumberCount, int prize, int winningCount) {
+    WinningEnum(int sameNumberCount, int prize) {
         this.sameNumberCount = sameNumberCount;
         this.prize = prize;
-        this.winningCount = winningCount;
     }
 
-    public String getWinningInfo(){
-        return sameNumberCount+WINNING_RESULT_PREFIX+prize+WINNING_RESULT_SUFFIX;
+    public String getWinningInfo() {
+        if (this == SECOND_PRIZE) {
+            return sameNumberCount + WINNING_RESULT_PREFIX + prize + WINNING_RESULT_SUFFIX;
+        }
+        return sameNumberCount + WINNING_RESULT_PREFIX + prize + WINNING_RESULT_SUFFIX;
     }
 }
