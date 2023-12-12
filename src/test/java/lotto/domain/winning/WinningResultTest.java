4bd474f4dec58ca7336e1e324e4eeee01d7a2b28
linirini <2001yerin@naver.com>
2023-12-13 01:58:28 +0900

    test : test for returning unmodifiable winning result

--- src/test/java/lotto/domain/winning/WinningResultTest.java
+++ src/test/java/lotto/domain/winning/WinningResultTest.java
@@ -1,6 +1,7 @@
 package lotto.domain.winning;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,15 @@ class WinningResultTest {
     void 총_상금_반환() {
         List<WinningEnum> winningEnums = getWinningEnums();
         assertThat(new WinningResult(winningEnums).getTotalPrize()).isEqualTo(1505000);
+    }
+
+    @DisplayName("불변 형태로 당첨 결과 목록을 반환한다.")
+    @Test
+    void 불변_당첨_결과_반환() {
+        List<WinningEnum> winningEnums = getWinningEnums();
+        assertThatThrownBy(
+                () -> new WinningResult(winningEnums).getWinnings()
+                        .put(WinningEnum.FIFTH_PRIZE, 1)).isInstanceOf(Exception.class);
     }
 
     private static List<WinningEnum> getWinningEnums() {
