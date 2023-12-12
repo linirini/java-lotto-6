f4659c70fc1e826cf232ec46914fc0d00aa77681
linirini <2001yerin@naver.com>
2023-12-13 02:19:22 +0900

    refactor : change access modifier for getter

--- src/main/java/lotto/domain/winning/WinningEnum.java
+++ src/main/java/lotto/domain/winning/WinningEnum.java
@@ -24,7 +24,7 @@ public enum WinningEnum {
         this.prize = prize;
     }
 
-    public int getSameNumberCount() {
+    private int getSameNumberCount() {
         return sameNumberCount;
     }
 
