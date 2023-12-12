ad4605e6f6fd35257e7fd574845d363762e2655d
linirini <2001yerin@naver.com>
2023-12-12 14:44:04 +0900

    feat : implmement createing winnings result

--- src/main/java/lotto/domain/winning/WinningResult.java
+++ src/main/java/lotto/domain/winning/WinningResult.java
@@ -3,13 +3,29 @@ package lotto.domain.winning;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class WinningResult {
 
     private final HashMap<WinningEnum,Integer> winnings;
 
     public WinningResult(HashMap<WinningEnum, Integer> winnings) {
-        this.winnings = winnings;
+        this.winnings = initWinnings();
+        updateWinnings(winnings);
+    }
+
+    private HashMap<WinningEnum, Integer> initWinnings() {
+        HashMap<WinningEnum, Integer> winnings = new HashMap<>();
+        for (WinningEnum winningEnum : WinningEnum.values()) {
+            winnings.put(winningEnum,0);
+        }
+        return winnings;
+    }
+
+    private void updateWinnings(HashMap<WinningEnum, Integer> winnings) {
+        for (Entry<WinningEnum, Integer> entry : winnings.entrySet()) {
+            this.winnings.put(entry.getKey(),this.winnings.get(entry.getKey())+1);
+        }
     }
 
     public int getPrize(){
