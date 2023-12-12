b1864185e4c01b285d802e227002af69c210472f
linirini <2001yerin@naver.com>
2023-12-12 13:05:45 +0900

    feat : implement input enums

--- src/main/java/lotto/view/InputEnum.java
+++ src/main/java/lotto/view/InputEnum.java
@@ -1,9 +1,12 @@
 package lotto.view;
 
 public enum InputEnum {
-    ;
 
-    private String message;
+    MONEY_INPUT("구입금액을 입력해 주세요."),
+    WINNING_LOTTO_INPUT("당첨 번호를 입력해 주세요."),
+    WINNING_BONUS_INPUT("보너스 번호를 입력해 주세요.");
+
+    private final String message;
 
     InputEnum(String message) {
         this.message = message;
