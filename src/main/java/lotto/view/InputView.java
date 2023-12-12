c4a5cccf76fc5ca489b4115998cff67a1f4202ff
linirini <2001yerin@naver.com>
2023-12-12 13:07:26 +0900

    feat : implement getting winning bonus number input

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,23 +2,27 @@ package lotto.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.view.InputEnum.MONEY_INPUT;
+import static lotto.view.InputEnum.WINNING_BONUS_INPUT;
 import static lotto.view.InputEnum.WINNING_LOTTO_INPUT;
 
 public class InputView {
 
-    public String inputMoney(){
+    public String inputMoney() {
         System.out.println(MONEY_INPUT.getMessage());
         String input = readLine();
         return input;
     }
 
-    public String inputWinningLotto(){
+    public String inputWinningLotto() {
         System.out.println(WINNING_LOTTO_INPUT.getMessage());
         String input = readLine();
         return input;
     }
 
-    public String inputWinningBonus(){
-        return null;
+    public String inputWinningBonus() {
+        System.out.println(WINNING_BONUS_INPUT.getMessage());
+        String input = readLine();
+        return input;
     }
+
 }
